ac07b0e2b042e1e8b9d336bace9adb660764602c
IndrachapaSenanayaka <senanayakeindrachapa@gmail.com>
2022-03-26 23:02:44 +0530

    backend was initialized

---
+++ hms-backend/src/app.js
@@ -0,0 +1,30 @@
+import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+import { connect } from "./utils/database.connection";
+import { logger } from './utils/logger';
+
+const app = express();
+const PORT = process.env.PORT || "8070";
+
+app.use(cors());
+app.use(express.json({limit: "20mb" }));
+
+
+app.listen(PORT, () => {
+    logger('info',`🚀 Server is up and running on PORT ${PORT}`);
+    connect();
+});
+
+app.get("/", (req, res, next) => {
+    res.send("<h2>🩺 Hospital Management System API</h2>");
+    next();
+});
+
+
+// const pharmacyItemsRouter = require("./api/routes/pharmacy_items.routes");
+// app.use("/pharmacyItem", pharmacyItemsRouter);
+
+
+
+
