7360798d9a02fb9434449b6266c6af8a7982b360
IndrachapaSenanayaka <senanayakeindrachapa@gmail.com>
2022-04-13 22:01:07 +0530

    images added

--- hms-frontend/src/App.js
+++ hms-frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import StockManager from './components/staff.components/stock_manager.components/Staff.stock_manager';
 
 
@@ -8,11 +8,7 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route path="/staff/stock-manager" component={StockManager}/>
-        </Switch>
-      </div>
+      <Route path="/staff/stock-manager" component={StockManager}/>
     </Router>
       
   );
--- hms-frontend/src/components/common.components/Logo.js
+++ hms-frontend/src/components/common.components/Logo.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './common_styles.css';
-import LogoImg from '../../images/logo.png';
+
 
 export default function Logo(){
 
     return(
         <div className="sidebar-item logo-container">
-              <img className="logoImg" alt="logo" src={LogoImg}/>
+              <img className="logoImg" alt="logo" src="/logo.png"/>
         </div>
     );
 }
