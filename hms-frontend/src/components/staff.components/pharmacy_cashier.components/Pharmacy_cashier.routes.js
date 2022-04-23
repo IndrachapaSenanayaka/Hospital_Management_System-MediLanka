914e6f7c277ee205ee17103b409bae92a44d7f92
IndrachapaSenanayaka <senanayakeindrachapa@gmail.com>
2022-04-23 10:16:34 +0530

    pharmacy cashier route was created

---
+++ hms-frontend/src/components/staff.components/pharmacy_cashier.components/Pharmacy_cashier.routes.js
@@ -0,0 +1,35 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import StaffLogo from "../Staff_logo";
+import './pharmacyCashierStyles.css';
+import './gridItemStyles.css';
+import './qrCodeScanner.css';
+import './shoppingCart.css';
+import './gridContainerStyles.css';
+
+export default function PharmacyCashier(){
+
+    return (
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/staff/pharmacy-cashier">
+                <div className="main-container">
+                  <div className="flex-box-container">
+                    <div className="flex-box sidebar-container">
+                      <StaffLogo/>
+                      
+                    </div>
+                    <div className="flex-box content-container">                    
+                      
+                            
+                    </div>
+                  </div>                
+                </div>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+          
+    );
+}
